7aab4af0eb33dfc52d260138958e77337eb4c7b0
Prateek Bhandari <prateekmbhandari@gmail.com>
2025-08-11 16:35:05 -0400

    ready to deploy backend

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,10 @@
 # e.g., backend/db.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/jobtracker.db")
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
